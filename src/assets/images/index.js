b1111e250a270f950138a282aaae315446ae8a81
Yann Kamche <yannkamche@gmail.com>
2024-02-01 01:48:43 +0100

    Assests images

---
+++ src/assets/images/index.js
@@ -0,0 +1,11 @@
+import image1 from './image1.png'
+import image2 from './image2.png'
+import image3 from './image3.png'
+import image4 from './image4.png'
+
+export {
+    image1,
+    image2,
+    image3,
+    image4
+}
