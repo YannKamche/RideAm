b980c484b866f8aa8180007fa2962dc4846feff5
Yann Kamche <yannkamche@gmail.com>
2024-02-02 02:18:45 +0100

    Added an attribute for the search button and its styles

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,15 +1,18 @@
 import classNames from 'classnames';
 
-const Button = ({ label, primaryBtn, signUp, normalBtn }) => {
+//import icon
+import {AiOutlineSearch} from 'react-icons/ai'
+
+const Button = ({ label, primaryBtn, signUp, normalBtn, searchBtn}) => {
   const buttonClasses = classNames(
     'py-[10px]',
     'px-[15px]',
     'cursor-pointer',
-    'max-sm:text-[12px]',
     'text-[14px]',
     'hover:bg-text-color',
     'hover:text-black-color',
     {
+      'flex justify-center w-full gap-1 self-center': searchBtn,
       'bg-secondary-color': !primaryBtn,
       'py-[3px] px-[10px] rounded-[3px]': primaryBtn && signUp,
       'bg-primary-color text-white-color hover:bg-secondary-color hover:text-black-color rounded-[5px]': primaryBtn,
@@ -19,7 +22,10 @@ const Button = ({ label, primaryBtn, signUp, normalBtn }) => {
     }
   );
 
-  return <button className={buttonClasses}>{label}</button>;
+  return <button className={buttonClasses}>
+    {searchBtn && <AiOutlineSearch className='text-xl'/>}
+    {label}
+    </button>;
 };
 
 export default Button;
